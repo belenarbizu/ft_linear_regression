a80fa5196e36ec2b6cc42f3a80689fdba843bef8
belen-applymed <158149022+belen-applymed@users.noreply.github.com>
2024-10-31 11:24:47 +0100

    added formulas info in predicted_price file

--- predict_price.py
+++ predict_price.py
@@ -1,6 +1,9 @@
 import sys
 
 def predict_price(theta0, theta1, mileage):
+    """
+    Function to calculate the predicted price
+    """
     return (theta0 + (theta1 * mileage))
 
 def check_input(mileage)-> int:
@@ -15,9 +18,16 @@ def check_input(mileage)-> int:
     return num
 
 def read_file():
-    file = open("data", "r")
-    s = file.read()
-    data = s.split(",")
+    """
+    Read and open the file and return the theta data
+    """
+    try:
+        file = open("data", "r")
+        s = file.read()
+        data = s.split(",")
+    except:
+        print("Couldn't read the data file")
+        sys.exit(1)
     return float(data[0]), float(data[1])
 
 def main():
@@ -26,7 +36,7 @@ def main():
     num = check_input(mileage)
     theta0, theta1 = read_file()
     estimate_price = predict_price(theta0, theta1, num)
-    print("The estimate price is", estimate_price)
+    print("The estimate price is", f'{estimate_price:.2f}')
 
 if __name__ == "__main__":
     main()
