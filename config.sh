b653412b44e7940b688f758f5c5ddbf7fff712ed
belen-applymed <158149022+belen-applymed@users.noreply.github.com>
2024-11-08 10:24:20 +0100

    config.sh

--- config.sh
+++ config.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 python3 -m venv env_python
 source env_python/bin/activate
-pip install -r requirements.txt
+if [ $VIRTUAL_ENV ]; then
+    pip install -r requirements.txt
+else
+    echo "The venv couldn't be created"
+fi
